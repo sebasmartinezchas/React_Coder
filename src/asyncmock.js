73341597f79e5bc3fb2d715d89e420fca2bff7a2
Andres Sebastian Martinez Chas <sebastianmartinezchas@gmail.com>
2022-06-09 01:22:41 -0300

    Agrego más productos y trabajo sobre ItemDetail

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -2,8 +2,8 @@ const productList = [
   {
     id: "1",
     name: "Campera Adidas",
-    priceStr:'Precio: ',
-    price: '$9.999',
+    priceStr:'Precio:$ ',
+    price: '9999',
     category: "hombre",
     img: "/images/campera_hombre_deportiva.png",
     stock: 15,
@@ -14,8 +14,8 @@ const productList = [
   {
     id: "2",
     name: "Zapatillas Forum",
-    priceStr:'Precio: ',
-    price: '$25.999',
+    priceStr:'Precio:$ ',
+    price: '25999',
     category: "mujer",
     img: "/images/zapatillas_forum_bold.jpg",
     stock: 10,
@@ -26,8 +26,8 @@ const productList = [
   {
     id: "3",
     name: "Conjunto de Buzo para Niño",
-    priceStr:'Precio: ',
-    price: '$9.999',
+    priceStr:'Precio:$ ',
+    price: '9999',
     category: "chicos",
     img: "/images/conjunto_buzo_niño.png",
     stock: 20,
@@ -38,14 +38,62 @@ const productList = [
   {
     id: "4",
     name: "Buzo con capucha River Plate",
-    priceStr:'Precio: ',
-    price: '$13.999',
+    priceStr:'Precio:$ ',
+    price: '13999',
     category: "hombre",
     img: "/images/buzo_River.png",
     stock: 38,
     description: "Buzo de River Plate con capucha",
     description2:
       "El estilo de fútbol de River Plate recibe el tratamiento de las 3 Tiras. adidas creó este buzo con capucha para los hinchas leales que nunca dejan de pensar en el club. El tejido de felpa francesa suave y la capucha forrada se encargan de brindar comodidad en todo momento. Un escudo clásico del club bordado en el pecho le pone un toque único.Este producto incorpora material reciclado como parte de nuestro anhelo de ponerle fin a la contaminación por plástico. Al comprar nuestros productos de algodón apoyás un cultivo de algodón más sostenible.",
+  },
+  {
+    id: "5",
+    name: "Remera Frozen 2",
+    priceStr:'Precio:$ ',
+    price: '4999',
+    category: "chicos",
+    img: "/images/remera_frozen.png",
+    stock: 18,
+    description: "Remera Frozen 2 Fans de Disney",
+    description2:
+      "Si tu pequeña es fan de Elsa, el personaje de Disney, esta remera adidas para niños se convertirá en una de sus favoritas. Confeccionada en tejido suave, presenta un diseño con cuello ancho y las 3 Tiras en las mangas que le imprimen un toque deportivo.",
+  },
+  {
+    id: "6",
+    name: "Botines Predator Edge.3",
+    priceStr:'Precio:$ ',
+    price: '18999',
+    category: "hombre",
+    img: "/images/botines_predator.png",
+    stock: 8,
+    description: "Botines Predator Edge.3",
+    description2:
+      "Amague. Fuerza. Control. Cuando tenés ventaja, la cancha está llena de posibilidades. Viví el deporte rey desde un nuevo ángulo con los adidas Predator. Estos botines te ayudan a dominar la pelota con el exterior Control Zone que incorpora zonas estratégicas con un estampado que mejora el agarre. En la parte inferior, una llamativa suela te mantiene en control durante el terreno firme. El cuello elástico de dos piezas en la parte superior ofrece un ajuste de gran sujeción.Hechos parcialmente con contenido reciclado generado a partir de desechos de producción, tales como, recortes de tela y los desechos domésticos postconsumo, para evitar un mayor impacto ambiental al producir contenido virgen",
+  },
+  {
+    id: "7",
+    name: "Calzas Running Essentials",
+    priceStr:'Precio:$ ',
+    price: '9499',
+    category: "mujer",
+    img: "/images/calzas_running.png",
+    stock: 28,
+    description: "Calzas Running Essentials 7/8",
+    description2:
+      "Alcanzá tu mejor marca personal con estas calzas adidas inspiradas en el tigre de agua. El diseño elástico se amolda a tus piernas para un look estilizado y favorecedor e incorpora tecnología de absorción AEROREADY para que puedas concentrarte en cumplir tus objetivos. El bolsillo en la cintura mantiene tus pertenencias seguras mientras completas el circuito en el gimnasio.Hecho con una serie de materiales reciclados y al menos 60% de contenido reciclado, este producto representa solo una de nuestras soluciones para acabar con los residuos plásticos.",
+  },
+  {
+    id: "8",
+    name: "Buzo con capucha Essentials",
+    priceStr:'Precio:$ ',
+    price: '12999',
+    category: "mujer",
+    img: "/images/buzo_mujer_essentials.png",
+    stock: 11,
+    description: "Buzo con capucha Essentials",
+    description2:
+      "Perfecto para después de entrenar. Este buzo con capucha adidas tiene un verdadero look deportivo con las 3 Tiras que van del hombro a la muñeca en ambas mangas. Los bolsillos canguro laterales y el tejido de felpa francesa ofrecen comodidad en todo momento.Este producto está hecho con material reciclado como parte de nuestro anhelo de ponerle fin a la contaminación por plástico.",
   },
 ];
 export const getProductsAsync = () => {
--- src/components/itemDetail/ItemDetail.js
+++ src/components/itemDetail/ItemDetail.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import '../itemDetail/ItemDetail.css'
 
 
 
-const ItemDetail = ({id, name,description2,img,description,price,priceStr}) => {
+const ItemDetail = ({ name,description2,img,description,price,priceStr}) => {
   return (
     <>
         <h1>{name}</h1>
-        <div><img src={img} alt={name}/></div>
+        <div><img src={img} alt={name} className='image-detail'/></div>
         <h2>{description}</h2>
-        <h3>{description2}</h3>
+        <p>{description2}</p>
         <h4>{priceStr}{price}</h4>
       
     
