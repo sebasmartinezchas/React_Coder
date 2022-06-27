aa22d086bdd7e116e255aa2ca6b03f4cadd62e65
Andres Sebastian Martinez Chas <sebastianmartinezchas@gmail.com>
2022-06-27 02:43:33 -0300

    Agrego actualizacion y creacion de orden mediante batch, limpio el carrito luego de generada la orden e invito al usuario a seguir navegando

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -1,11 +1,24 @@
 import React, { useState, useContext } from "react";
-import { addDoc, collection } from "firebase/firestore";
+import { Link } from "react-router-dom";
+import {
+  addDoc,
+  collection,
+  writeBatch,
+  getDocs,
+  query,
+  where,
+  documentId,
+  doc,
+} from "firebase/firestore";
 import { db } from "../../services/firebase/index";
 import CartContext from "../../context/CartContext";
 import "./Checkout.css";
+import NotificationContext from "../../notification/Notification";
 
 const Checkout = () => {
-  const { cart, totalPurchase } = useContext(CartContext);
+  const { cart, totalPurchase,clearCart,totalQuantityInCart } = useContext(CartContext);
+  const setNotification = useContext(NotificationContext);
+  const[loading,setLoading]=useState(false)
   const [data, setData] = useState({
     fullname: "",
     email: "",
@@ -22,19 +35,69 @@ const Checkout = () => {
     e.preventDefault();
   };
   const handleCreateOrder = () => {
+    setLoading(true)
     const objOrder = {
       buyer: data,
       items: cart,
       total: totalPurchase,
     };
-  
-    const collectionRef = collection(db, "orders");
+    const batch = writeBatch(db);
+    const ids = cart.map((product) => product.id);
+    const outStock = [];
+    const prodColRef = collection(db, "productList");
+    getDocs(query(prodColRef, where(documentId(), "in", ids)))
+      .then((response) => {
+        response.docs.forEach((doc) => {
+          const dataDoc = doc.data();
+          const prod = cart.find((prod) => prod.id === doc.id);
+          const productQuantity = prod.quantity;
 
-    addDoc(collectionRef,objOrder).then(({ id } )=>{
-        console.log(id)
-
-    })
+          if (dataDoc.stock >= productQuantity) {
+            batch.update(doc.ref, { stock: dataDoc.stock - productQuantity });
+          } else {
+            outStock.push({ id: doc.id, ...dataDoc });
+          }
+        });
+      })
+      .then(() => {
+        if (outStock.length === 0) {
+          const collectionRef = collection(db, "orders");
+          return addDoc(collectionRef, objOrder);
+        } else {
+          return Promise.reject({ type: "out_of_stock", products: outStock });
+        }
+      })
+      .then(({ id }) => {
+        batch.commit();
+        setNotification(
+          "success",
+          `Su orden se creo correctamente. El Id de su orden es: ${id}`
+        );
+      })
+      .catch((error) => {
+        if (error.type === "out_of_stock") {
+          setNotification("error", "Hay productos que no tienen stock");
+        } else {
+          console.log(error);
+        }
+      }).finally(()=>{
+        setLoading(false)
+        clearCart()
+      })
   };
+  if(loading){
+    return <h1>Se esta generando su orden</h1>
+  }
+  if (!totalQuantityInCart) {
+    return (
+      <div>
+        <h1>No hay productos en su carrito pendientes de comprar</h1>
+        <Link to="/">
+          <button>Conozca mas de nuestros productos</button>
+        </Link>
+      </div>
+    );
+  }
   return (
     <>
       <h1> Ingrese sus datos y finalize su orden de compra</h1>
